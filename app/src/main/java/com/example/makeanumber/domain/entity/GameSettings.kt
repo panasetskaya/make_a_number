501a2c628a684998eb75c050acfcfe655c17b878
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-05-24 15:55:13 +0800

    implement data layer

--- app/src/main/java/com/example/makeanumber/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/makeanumber/domain/entity/GameSettings.kt
@@ -1,8 +1,8 @@
 package com.example.makeanumber.domain.entity
 
 data class GameSettings (
-    val gameTimeSeconds: Int,
     val maxSumValue: Int,
     val minCountOfRightAnswers: Int,
-    val minPercentOfRightAnswers: Int
+    val minPercentOfRightAnswers: Int,
+    val gameTimeSeconds: Int,
         )
